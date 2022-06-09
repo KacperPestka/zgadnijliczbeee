68149998c8551da4e77d29766cb53b16de17dd01
3110242 <3110242@TE-GDA-D-D37H15>
2022-06-09 12:39:03 +0200

    Dodaj pliki projektów.

---
+++ zgadnijliczhbeee/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace MyFirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            bool playAgain = true;
+            int min = 1;
+            int max = 10;
+            int guess;
+            int number;
+            int guesses;
+            String response;
+
+            while (playAgain)
+            {
+                guess = 0;
+                guesses = 0;
+                response = "";
+                number = random.Next(min, max + 1);
+
+                while (guess != number)
+                {
+                    Console.WriteLine("Wybierz liczbe " + min + " - " + max + " : ");
+                    guess = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Guess: " + guess);
+
+                    if (guess > number)
+                    {
+                        Console.WriteLine(guess + "Za wysoka");
+                    }
+                    else if (guess < number)
+                    {
+                        Console.WriteLine(guess + " Za niska liczba");
+                    }
+                    guesses++;
+                }
+                Console.WriteLine("Number: " + number);
+                Console.WriteLine("Wygrałes");
+                Console.WriteLine("Guesses: " + guesses);
+
+            }
+
+            Console.WriteLine("Koniec");
+
+            Console.ReadKey();
+        }
+    }
+}
